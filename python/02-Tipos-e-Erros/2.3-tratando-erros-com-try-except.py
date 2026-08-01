ea224750440d7e4a2ba9a80b47f1e07251aa3846
Marcelo Tonussi <marceloalesp@hotmail.com>
2026-08-01 18:54:40 -0400

    feat: adiciona novos códigos de estudo

---
+++ python/02-Tipos-e-Erros/2.3-tratando-erros-com-try-except.py
@@ -0,0 +1,95 @@
+##
+
+'''
+Temos 3 formas de tratamento: 
+    except:
+    except Exception: 
+    except Exception as e:
+
+1) - except:
+Aqui tratamos os erros de forma totalmente ampla. Pode ser qualquer erro.
+Caso ocorra qualquer tipo de erro, entrará neste bloco do except.
+O except puro captura absolutamente todos os tipos de erros e sinais do sistema (equivalente a BaseException)
+Pega qualquer coisa que interrompa o código, incluindo fechar o programa à força.
+
+Problema grave: 
+    Captura comandos como KeyboardInterrupt (quando você aperta Ctrl + C para parar um loop infinito) e SystemExit.
+
+Efeito colateral: 
+    Pode esconder erros reais do sistema ou impedir você de desligar um script travado.
+
+2) - except Exception:
+Aqui ele captura apenas os erros normais de código, ignorando interrupções cruciais do usuário ou do sistema operacional.
+Captura segura: 
+    Pega somente os erros que herdam da classe padrão Exception 
+    (como ValueError, TypeError, ZeroDivisionError, etc.).
+
+Respeita o sistema: 
+    Deixa passar avisos vitais de saída e interrupções manuais (KeyboardInterrupt), 
+    permitindo o controle adequado do fluxo do programa.
+
+Boa prática: 
+    É considerado mais seguro, embora o ideal ainda seja especificar o erro exato que você espera (ex: except ValueError:).
+
+3) - except Exception as e:
+Neste caso, assim que um erro ocorre, além de tratarmos o erro,  ele é armazenado na variável "e" (pode utilizar a variável que quiser).
+Permite que você acesse os detalhes da falha.
+
+O que muda com o "as e": 
+Acesso à mensagem: 
+    Você consegue ler e exibir a mensagem de erro exata gerada pelo Python.
+Inspeção: 
+    Permite descobrir o tipo específico do erro ou investigar a causa do problema durante a execução.
+
+Logs e depuração: 
+    É essencial para gravar os erros em arquivos de texto (logs) para análise posterior, 
+    sem interromper o funcionamento do sistema.
+'''
+
+# 1) Código que quebra se o usuário digitar errado
+gols = int(input("Quantos gols o Brasil fez? "))
+print(f"O Brasil fez {gols} gols.")
+
+#--------------------------------------------------------------
+
+# 2) ValueError acontecendo
+# numero = int("três")
+# print(numero)
+
+#--------------------------------------------------------------
+
+# 3) Tratando erro ao digitar os gols do Brasil
+try:
+    gols = int(input("Quantos gols o Brasil fez? "))
+    print(f"O Brasil fez {gols} gols.")
+
+except ValueError:
+    print("Você precisa digitar um número inteiro") # esse print é opcional
+
+#--------------------------------------------------------------
+
+# 4) Analisando os gols do Brasil usando try/except com if
+try:
+    gols = int(input("Quantos gols o Brasil marcou?"))
+
+    if gols >= 3:
+        print("O Brasil fez muitos gols!")
+    elif gols ==1 or gols == 2:
+        print("O Brasil marcou, mas poderia ter feito mais.")
+    else:
+        print(" O Brasil não marcou gols.")
+
+except ValueError:
+    print("Você precisa digitar um número inteiro.")
+
+#--------------------------------------------------------------
+
+# 5) Pedindo os gols até o usuário digitar um número válido
+numero_valido = False
+
+while numero_valido == False:
+    try:
+        gols = int(input("Quantos gols o Brasil fez? "))
+        numero_valido = True
+    except ValueError:
+        print("Valor inválido. Digite um número inteiro. (ex: 1, 2, 4..)")
